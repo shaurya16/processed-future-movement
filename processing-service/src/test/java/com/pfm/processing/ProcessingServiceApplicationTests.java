a242b7535d0262bb31bb562528a7f63112d8bdd4
Shaurya Srivastava <shaurya16@gmail.com>
2026-08-12 18:27:15 +0800

    Revert "feat(processing-service): require PFM_TOPIC, drop hardcoded topic default"
    This reverts commit a1ef0ff82875e647614628009f1ab4fe6cd66794.

--- processing-service/src/test/java/com/pfm/processing/ProcessingServiceApplicationTests.java
+++ processing-service/src/test/java/com/pfm/processing/ProcessingServiceApplicationTests.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        properties = {"spring.kafka.streams.auto-startup=false", "processing.topic=future-transactions"})
+        properties = "spring.kafka.streams.auto-startup=false")
 class ProcessingServiceApplicationTests {
 
     @Autowired
